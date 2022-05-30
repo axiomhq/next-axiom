0c7c51e6f673083cd1ab6dac0e486b048d588a35
Islam Shehata <islam@axiom.co>
2022-05-30 13:02:22 +0200

    extract withAxiom to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
+import { NextConfig } from 'next';
+import { EndpointType, getIngestURL } from './config';
 export { reportWebVitals } from './webVitals';
 export { log } from './logger';
+
+export function withAxiom(nextConfig: NextConfig): NextConfig {
+  return {
+    ...nextConfig,
+    rewrites: async () => {
+      const rewrites = await nextConfig.rewrites?.();
+
+      const ingestEndpoint = getIngestURL(EndpointType.webVitals);
+      if (!ingestEndpoint) {
+        return rewrites || []; // nothing to do
+      }
+
+      const axiomRewrites = [
+        {
+          source: '/axiom/web-vitals',
+          destination: ingestEndpoint,
+        },
+      ];
+
+      if (!rewrites) {
+        return axiomRewrites;
+      } else if (Array.isArray(rewrites)) {
+        return rewrites.concat(axiomRewrites);
+      } else {
+        rewrites.afterFiles = rewrites.afterFiles.concat(axiomRewrites);
+        return rewrites;
+      }
+    },
+  };
+}
--- src/webVitals.ts
+++ src/webVitals.ts
@@ -1,40 +1,8 @@
-import { NextConfig } from 'next';
 import { NextWebVitalsMetric } from 'next/app';
-import { EndpointType, getIngestURL } from './config';
 export { log } from './logger';
 const _debounce = require('lodash/debounce');
 
 export declare type WebVitalsMetric = NextWebVitalsMetric & { route: string };
-
-export function withAxiom(nextConfig: NextConfig): NextConfig {
-  return {
-    ...nextConfig,
-    rewrites: async () => {
-      const rewrites = await nextConfig.rewrites?.();
-
-      const ingestEndpoint = getIngestURL(EndpointType.webVitals);
-      if (!ingestEndpoint) {
-        return rewrites || []; // nothing to do
-      }
-
-      const axiomRewrites = [
-        {
-          source: '/axiom/web-vitals',
-          destination: ingestEndpoint,
-        },
-      ];
-
-      if (!rewrites) {
-        return axiomRewrites;
-      } else if (Array.isArray(rewrites)) {
-        return rewrites.concat(axiomRewrites);
-      } else {
-        rewrites.afterFiles = rewrites.afterFiles.concat(axiomRewrites);
-        return rewrites;
-      }
-    },
-  };
-}
 
 const debounceSendMetrics = _debounce(() => sendMetrics(), 1000);
 let collectedMetrics: WebVitalsMetric[] = [];
