661f46f0c890182c6426499b07063d7a4ba06e4c
Arne Bahlo <arne@axiom.co>
2022-06-03 14:34:23 +0200

    Make log funcs async

--- src/logger.ts
+++ src/logger.ts
@@ -1,25 +1,12 @@
 import fetch from 'cross-fetch';
 import { EndpointType, getIngestURL } from './config';
 
-let collectedLogs: any[] = [];
-
-function _log(level: string, message: string, args: any = {}) {
+async function _log(level: string, message: string, args: any = {}) {
   const logEvent = { level, message, _time: new Date(Date.now()).toISOString() };
   if (Object.keys(args).length > 0) {
     logEvent['fields'] = args;
   }
-  collectedLogs.push(logEvent);
-  sendLogs();
-}
 
-export const log = {
-  debug: (message: string, args: any = {}) => _log('debug', message, args),
-  info: (message: string, args: any = {}) => _log('info', message, args),
-  warn: (message: string, args: any = {}) => _log('warn', message, args),
-  error: (message: string, args: any = {}) => _log('error', message, args),
-};
-
-function sendLogs() {
   let url = '/_axiom/logs';
   // check if running in nodejs and add baseURL so that
   // fetch works correctly
@@ -27,11 +14,18 @@ function sendLogs() {
   if (!isBrowser) {
     url = getIngestURL(EndpointType.logs);
   }
-  const body = JSON.stringify(collectedLogs);
+  const body = JSON.stringify([logEvent]);
 
   if (isBrowser && navigator.sendBeacon) {
     navigator.sendBeacon(url, body);
   } else {
-    fetch(url, { body, method: 'POST', keepalive: true });
+    await fetch(url, { body, method: 'POST', keepalive: true });
   }
 }
+
+export const log = {
+  debug: async (message: string, args: any = {}) => await _log('debug', message, args),
+  info: async (message: string, args: any = {}) => await _log('info', message, args),
+  warn: async (message: string, args: any = {}) => await _log('warn', message, args),
+  error: async (message: string, args: any = {}) => await _log('error', message, args),
+};
