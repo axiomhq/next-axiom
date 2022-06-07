b79d0256c4246b51aa71cb9f8745cb2f90b69b24
Arne Bahlo <arne@axiom.co>
2022-06-07 08:36:55 +0200

    Replace lodash/debounce with custom impl
    NextJS middleware can't deal with node modules, so we need to be careful
    with adding them.

--- src/webVitals.ts
+++ src/webVitals.ts
@@ -4,16 +4,23 @@ import { isBrowser, proxyPath } from './config';
 export { log } from './logger';
 
 const url = `${proxyPath}/web-vitals`;
-const _debounce = require('lodash/debounce');
+
+const debounce = (fn: Function, ms = 300) => {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  };
+};
 
 export declare type WebVitalsMetric = NextWebVitalsMetric & { route: string };
 
-const debounceSendMetrics = _debounce(() => sendMetrics(), 1000);
+const debouncedSendMetrics = debounce(sendMetrics, 1000);
 let collectedMetrics: WebVitalsMetric[] = [];
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
   collectedMetrics.push({ route: window.__NEXT_DATA__?.page, ...metric });
-  debounceSendMetrics();
+  debouncedSendMetrics();
 }
 
 function sendMetrics() {
