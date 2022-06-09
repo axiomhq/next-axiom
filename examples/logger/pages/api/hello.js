ed2534b2b02262664e128d3cabd38a2f32a880a0
Arne Bahlo <arne@axiom.co>
2022-06-09 17:18:50 +0200

    Use waitUntil, log.flush() in middleware, function

--- examples/logger/pages/api/hello.js
+++ examples/logger/pages/api/hello.js
@@ -3,5 +3,6 @@ import { log } from 'next-axiom'
 
 export default async function handler(req, res) {
   log.info('Hello from function', { url: req.url });
+  await log.flush()
   res.status(200).json({ name: 'John Doe' })
 }
