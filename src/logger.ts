99adfa7e4c2014c8b317ba3579792dff5b714c5a
Islam Shehata <islam@axiom.co>
2022-06-09 10:47:46 +0200

    listen on beforeExit and flush logs

--- src/logger.ts
+++ src/logger.ts
@@ -5,6 +5,22 @@ const url = isBrowser ? `${proxyPath}/logs` : getIngestURL(EndpointType.logs);
 const debouncedSendLogs = debounce(sendLogs, 1000);
 let logEvents: any[] = [];
 
+// if is running on node, print to stdout, output will be pickedup with vercel
+// otherwise send as json.
+if (!isBrowser) {
+  process.on('beforeExit', async () => {
+    await sendLogs()
+    process.exit(0) // if you don't close yourself this will run forever
+  });
+
+  ['SIGINT', 'SIGTERM'].forEach(function (sig) {
+    process.on(sig, function () {
+        debounce(sendLogs, 0);
+        console.log('signal: ' + sig);
+    });
+  });
+}
+
 function _log(level: string, message: string, args: any = {}) {
   if (!url) {
     console.warn('axiom: NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT is not defined');
@@ -14,14 +30,6 @@ function _log(level: string, message: string, args: any = {}) {
   const logEvent = { level, message, _time: new Date(Date.now()).toISOString() };
   if (Object.keys(args).length > 0) {
     logEvent['fields'] = args;
-  }
-
-  // if is running on node, print to stdout, output will be pickedup with vercel
-  // otherwise send as json
-  if (!isBrowser) {
-    const body = JSON.stringify(logEvent);
-    console.log('AXIOM::LOG=' + body);
-    return;
   }
 
   logEvents.push(logEvent);
@@ -35,13 +43,16 @@ export const log = {
   error: (message: string, args: any = {}) => _log('error', message, args),
 };
 
-function sendLogs() {
+async function sendLogs() {
+  if (!logEvents.length) { 
+    return;
+  }
   const body = JSON.stringify(logEvents);
 
   if (isBrowser && navigator.sendBeacon) {
     navigator.sendBeacon(url, body);
   } else {
-    fetch(url, { body, method: 'POST', keepalive: true });
+    await fetch(url, { body, method: 'POST', keepalive: true });
   }
 
   // clear logs after they are pushed
