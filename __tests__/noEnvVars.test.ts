d91280c02e6f72f80907de0102887b1b3eb5bab4
Islam Shehata <islam@axiom.co>
2022-06-21 17:11:41 +0200

    fix: avoid network errors if envVars are not set

---
+++ __tests__/noEnvVars.test.ts
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// clear Axiom env vars
+process.env.AXIOM_INGEST_ENDPOINT = '';
+process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT = '';
+import { NextWebVitalsMetric } from 'next/app';
+import { log } from '../src/logger';
+import { reportWebVitals } from '../src/webVitals';
+
+global.fetch = jest.fn() as jest.Mock;
+const mockedLog = jest.spyOn(global.console, 'log').mockImplementation();
+
+test('sending logs on localhost should fallback to console', () => {
+  log.info('hello, world!');
+  jest.advanceTimersByTime(1000);
+  expect(fetch).toHaveBeenCalledTimes(0);
+  expect(mockedLog).toHaveBeenCalledTimes(1);
+});
+
+test('webVitals should not be sent when envVars are not set', () => {
+  const metric: NextWebVitalsMetric = { id: '1', startTime: 1234, value: 1, name: 'FCP', label: 'web-vital' };
+  reportWebVitals(metric);
+  jest.advanceTimersByTime(1000);
+  expect(fetch).toHaveBeenCalledTimes(0);
+});
