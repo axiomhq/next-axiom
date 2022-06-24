e3410c8d78b4e75d76075837193235dac040759c
Islam Shehata <islam@axiom.co>
2022-06-24 14:55:40 +0200

    enhance: pretty print on localhost since we are falling back to console when no env vars are available, we decided to improve how the logs look when they are printed on the browser and on the terminal.

--- src/logger.ts
+++ src/logger.ts
@@ -13,7 +13,7 @@ function _log(level: string, message: string, args: any = {}) {
     if (Object.keys(args).length) {
       fields = args;
     }
-    console.log(`${level} - ${message}`, fields);
+    prettyPrint(level, message, fields);
     return;
   }
 
@@ -57,4 +57,48 @@ async function sendLogs() {
   } catch (e) {
     console.error(`Failed to send logs to Axiom: ${e}`);
   }
+}
+
+const levelColors = {
+  info: {
+    terminal: '32',
+    browser: 'lightgreen',
+  },
+  debug: {
+    terminal: '36',
+    browser: 'lightblue',
+  },
+  warn: {
+    terminal: '33',
+    browser: 'yellow',
+  },
+  error: {
+    terminal: '31',
+    browser: 'red',
+  },
+};
+
+function prettyPrint(level: string, message: string, fields: any = {}, indent = 2) {
+  // print indented message, instead of [object]
+  // We use the %o modifier instead of JSON.stringify because stringify will print the
+  // object as normal text, it loses all the functionality the browser gives for viewing
+  // objects in the console, such as expanding and collapsing the object.
+  let msgString = '';
+  let args = [level, message];
+  const hasFields = Object.keys(fields).length > 0;
+
+  if (isBrowser) {
+    msgString = '%c%s - %s';
+    args = [`color: ${levelColors[level].browser};`, ...args];
+  } else {
+    msgString = `\x1b[${levelColors[level].terminal}m%s\x1b[0m - %s`;
+  }
+  // we check if the fields object is not empty, otherwise its printed as <empty string>
+  // or just "".
+  if (hasFields) {
+    msgString += ' %o';
+    args.push(fields);
+  }
+
+  console.log.apply(console, [msgString, ...args]);
 }
