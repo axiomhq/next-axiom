e1c4bcc2171065e6acc8d5d5881d648c2a31074a
Arne Bahlo <arne@axiom.co>
2022-07-04 10:47:33 +0200

    Support attaching fields to an intermediate logger

--- __tests__/log.test.ts
+++ __tests__/log.test.ts
@@ -5,10 +5,11 @@
 process.env.AXIOM_INGEST_ENDPOINT = 'https://example.co/api/test';
 import { log } from '../src/logger';
 
-global.fetch = jest.fn() as jest.Mock;
 jest.useFakeTimers();
 
 test('sending logs from browser', async () => {
+  global.fetch = jest.fn() as jest.Mock;
+
   log.info('hello, world!');
   expect(fetch).toHaveBeenCalledTimes(0);
 
@@ -20,4 +21,22 @@ test('sending logs from browser', async () => {
 
   await log.flush();
   expect(fetch).toHaveBeenCalledTimes(2);
+});
+
+test('with', async () => {
+  global.fetch = jest.fn() as jest.Mock;
+
+  const logger = log.with({ foo: 'bar' });
+  logger.info('hello, world!', { bar: 'baz' });
+  expect(fetch).toHaveBeenCalledTimes(0);
+
+  jest.advanceTimersByTime(1000);
+  expect(fetch).toHaveBeenCalledTimes(1);
+  const payload = JSON.parse((fetch as jest.Mock).mock.calls[0][1].body);
+  expect(payload.length).toBe(1);
+  const fst = payload[0];
+  expect(fst.level).toBe('info');
+  expect(fst.message).toBe('hello, world!');
+  expect(fst.fields.foo).toBe('bar');
+  expect(fst.fields.bar).toBe('baz');
 });
