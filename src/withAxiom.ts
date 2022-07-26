6a47b5ba248a2a69b15fdb0a64df90ef6e56cdb8
Islam Shehata <islam@axiom.co>
2022-07-26 17:15:46 +0200

    get lambda res status from wrappedRes

--- src/withAxiom.ts
+++ src/withAxiom.ts
@@ -115,7 +115,7 @@ function withAxiomNextApiHandler(handler: NextApiHandler): NextApiHandler {
 
     try {
       await handler(axiomRequest, wrappedRes);
-      logger.attachResponseStatus(200);
+      logger.attachResponseStatus(wrappedRes.statusCode);
       await logger.flush();
       await Promise.all(allPromises);
     } catch (error) {
