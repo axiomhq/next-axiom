ac33182cf70bf7bd3088f2d53d0ffda672b51a9e
Islam Shehata <islam@axiom.co>
2022-10-03 10:41:12 +0200

    debug isNetlify

--- src/shared.ts
+++ src/shared.ts
@@ -5,7 +5,7 @@ import { RequestReport } from './logger';
 export const proxyPath = '/_axiom';
 // these values are defined here so that it works with frontend, since they are resolved at run time
 const isVercel = process.env.AXIOM_INGEST_ENDPOINT || process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT;
-const isNetlify = typeof process.env.NETLIFY != `undefined` ? true : false;
+const isNetlify = process.env.NETLIFY;
 
 export const isBrowser = typeof window !== 'undefined';
 export const isNoPrettyPrint = process.env.AXIOM_NO_PRETTY_PRINT == 'true' ? true : false;
