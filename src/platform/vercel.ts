00d8150bed5a8745ce6a45a0254c18fc969dc479
Islam Shehata <islam@axiom.co>
2022-10-04 11:26:21 +0200

    debug axiom url

--- src/platform/vercel.ts
+++ src/platform/vercel.ts
@@ -10,15 +10,17 @@ export default class VercelConfig extends GenericConfig implements PlatformConfi
   isEnvVarsSet =
     process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT != undefined || process.env.AXIOM_INGEST_ENDPOINT != undefined;
   region = process.env.VERCEL_REGION || undefined;
-  axiomUrl = ingestEndpoint || '';
   environment = process.env.VERCEL_ENV || process.env.NODE_ENV;
   token = undefined;
+  axiomUrl = '';
 
   constructor () {
     super();
-  }
+    this.axiomUrl = ingestEndpoint || '';  
+}
 
   getIngestURL(t: EndpointType) {
+    console.log('axiom url =>', process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT, this.axiomUrl)
     const url = new URL(this.axiomUrl);
     // attach type query param based on passed EndpointType
     url.searchParams.set('type', t.toString());
