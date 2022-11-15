c8392a766765f98ea4a46b6ee81e1d5d5fe5f999
Islam Shehata <islam@axiom.co>
2022-11-15 11:09:16 +0200

    debug process.env

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,8 @@ import { envVarExists } from './shared';
 const isVercel = envVarExists('AXIOM_INGEST_ENDPOINT') || envVarExists('NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT');
 const isNetlify = envVarExists('NETLIFY') && process.env.NETLIFY == 'true';
 
+console.log(process.env)
+
 if (isVercel) {
     console.log('vercel is used')
 } else if (isNetlify) {
