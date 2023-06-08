25a239b2ff3c3b2fffbc62dc90bfb786b3668df8
Islam Shehata <islam@axiom.co>
2023-06-08 18:03:31 +0300

    make use of useEffect to flush logger

--- packages/next-axiom/src/components.tsx
+++ packages/next-axiom/src/components.tsx
@@ -1,7 +1,18 @@
 'use client';
-import { useReportWebVitals } from './hooks';
+import { usePathname } from 'next/navigation';
+import { useLogger, useReportWebVitals } from './hooks';
+import { useEffect } from 'react';
 
 export function AxiomWebVitals() {
   useReportWebVitals();
+
+  // TODO: this could be used to flush logger whenever route changes
+  const pathname = usePathname();
+  const logger = useLogger();
+  useEffect(() => {
+    return async () => {
+      await logger.flush();
+    };
+  }, [pathname]);
   return null;
 }
