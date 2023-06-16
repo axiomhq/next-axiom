978d0c16a551469113128503d423c67eab207619
Islam Shehata <islam@axiom.co>
2023-06-16 18:44:34 +0300

    remove *.js from gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+};
---
+++ e2e/nextjs-app/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const { withAxiom } = require('next-axiom')
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withAxiom(nextConfig);
