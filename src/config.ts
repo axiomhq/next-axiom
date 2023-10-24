ff234f3e7a1e8d26f49616cad69310a08b16a4b2
Islam Shehata <islam@axiom.co>
2023-10-24 17:30:13 +0300

    add web-worker to example to test functionality

--- src/config.ts
+++ src/config.ts
@@ -3,14 +3,17 @@ import VercelConfig from './platform/vercel';
 import NetlifyConfig from './platform/netlify';
 
 declare global {
-  var WebSocketPair: any; // Non-standard global only used on Cloudflare: https://developers.cloudflare.com/workers/runtime-apis/websockets
+  var WorkerGlobalScope: any; // Non-standard global only used on Cloudflare: https://developers.cloudflare.com/workers/runtime-apis/websockets
 }
 
 export const Version = require('../package.json').version;
 export const isVercel = process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT || process.env.AXIOM_INGEST_ENDPOINT;
 export const isNetlify = process.env.NETLIFY == 'true';
-export const isBrowser =
-  typeof window !== 'undefined' || (typeof self !== 'undefined' && typeof Cloudflare === 'undefined');
+export const isWebWorker =
+  typeof self !== 'undefined' &&
+  typeof globalThis.WorkerGlobalScope !== 'undefined' &&
+  self instanceof WorkerGlobalScope;
+export const isBrowser = typeof window !== 'undefined' || isWebWorker;
 
 // Detect the platform provider, and return the appropriate config
 // fallback to generic config if no provider is detected
