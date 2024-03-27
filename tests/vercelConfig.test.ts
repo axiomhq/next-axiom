10de2dd16de439ac96f188c485f0583903cb44b9
Islam Shehata <islam@axiom.co>
2024-03-27 13:15:30 +0200

    utilize log drain on vercel functions

--- tests/vercelConfig.test.ts
+++ tests/vercelConfig.test.ts
@@ -1,6 +1,7 @@
 import { test, expect, vi } from 'vitest';
 import { config } from '../src/config';
 import { EndpointType } from '../src/shared';
+import { Logger } from '../src/logger';
 
 vi.hoisted(() => {
   process.env.NEXT_PUBLIC_AXIOM_URL = undefined;
@@ -13,4 +14,30 @@ test('reading vercel ingest endpoint', () => {
 
   url = config.getIngestURL(EndpointType.logs);
   expect(url).toEqual('https://api.axiom.co/v1/integrations/vercel?type=logs');
+});
+
+test('logging to console when running on lambda', async () => {
+  vi.useFakeTimers();
+  const mockedConsole = vi.spyOn(console, 'log');
+  const time = new Date(Date.now()).toISOString();
+
+  const logger = new Logger({
+    source: 'lambda',
+  });
+
+  logger.info('hello, world!');
+
+  await logger.flush();
+  expect(mockedConsole).toHaveBeenCalledTimes(1);
+  expect(mockedConsole).toHaveBeenCalledWith({
+    _time: time,
+    fields: {},
+    level: 'info',
+    message: 'hello, world!',
+    vercel: {
+      environment: 'test',
+      region: undefined,
+      source: 'lambda',
+    },
+  });
 });
