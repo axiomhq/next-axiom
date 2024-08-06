9aa9eee002bc39cba186cc26e80ba3bed99bfdc8
Islam Shehata <islam@axiom.co>
2024-08-06 19:49:57 +0300

    Add fields to example api route logs for testing

--- examples/logger/app/api/edge/route.ts
+++ examples/logger/app/api/edge/route.ts
@@ -3,6 +3,10 @@ import { AxiomRequest, withAxiom } from 'next-axiom';
 export const runtime = 'edge';
 
 export const GET = withAxiom(async (req: AxiomRequest) => {
-  req.log.info('fired from edge route');
+  req.log.info('fired from edge route', {
+    method: req.method,
+    author: 'Islam Sheata',
+    purpose: 'testing',
+  });
   return new Response('Hello, Next.js!');
 });
--- examples/logger/app/api/lambda/route.ts
+++ examples/logger/app/api/lambda/route.ts
@@ -3,6 +3,10 @@ import { AxiomRequest, withAxiom } from 'next-axiom';
 export const runtime = 'nodejs';
 
 export const GET = withAxiom(async (req: AxiomRequest) => {
-  req.log.info('axiom lambda route');
+  req.log.info('axiom lambda route', {
+    method: req.method,
+    author: 'Islam Sheata',
+    purpose: 'testing',
+  });
   return new Response('Hello, Next.js!');
 });
