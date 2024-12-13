c1003a9efcc4c86daad61d94381a81ffb9f484ec
Islam Shehata <islam@axiom.co>
2024-12-13 17:06:15 +0200

    lint: fix format errors

--- src/withAxiom.ts
+++ src/withAxiom.ts
@@ -93,10 +93,10 @@ export function withAxiomRouteHandler(handler: NextHandler, config?: AxiomRouteH
       region,
       details: Array.isArray(config?.logRequestDetails)
         ? (Object.fromEntries(
-          Object.entries(requestDetails as RequestJSON).filter(([key]) =>
-            (config?.logRequestDetails as (keyof RequestJSON)[]).includes(key as keyof RequestJSON)
-          )
-        ) as RequestJSON)
+            Object.entries(requestDetails as RequestJSON).filter(([key]) =>
+              (config?.logRequestDetails as (keyof RequestJSON)[]).includes(key as keyof RequestJSON)
+            )
+          ) as RequestJSON)
         : requestDetails,
     };
 
@@ -136,24 +136,24 @@ export function withAxiomRouteHandler(handler: NextHandler, config?: AxiomRouteH
       // capture request endTime first for more accurate reporting
       report.endTime = new Date().getTime();
       // set default values for statusCode and logLevel
-      let statusCode = 500
-      let logLevel = LogLevel.error
+      let statusCode = 500;
+      let logLevel = LogLevel.error;
       // handle navigation errors like notFound and redirect
       if (error instanceof Error) {
         if (error.message === 'NEXT_NOT_FOUND') {
-          logLevel = config?.NotFoundLogLevel ?? LogLevel.warn
-          statusCode = 404
+          logLevel = config?.NotFoundLogLevel ?? LogLevel.warn;
+          statusCode = 404;
         } else if (error.message === 'NEXT_REDIRECT') {
-          logLevel = config?.RedirectLogLevel ?? LogLevel.info
+          logLevel = config?.RedirectLogLevel ?? LogLevel.info;
           // according to Next.js docs, values are: 307 (Temporary) or 308 (Permanent)
           // see: https://nextjs.org/docs/app/api-reference/functions/redirect#why-does-redirect-use-307-and-308
           // extract status code from digest, if exists
           const e: Error & { digest?: string } = error;
           if (e.digest) {
             const d = e.digest.split(';');
-            statusCode = parseInt(d[3])
+            statusCode = parseInt(d[3]);
           } else {
-            statusCode = 307
+            statusCode = 307;
           }
         }
       }
