a3ec83a091421bb4dd3eefbce293a183b232be55
Gabriel De Andrade <gabrieldeandradeleal@gmail.com>
2024-12-15 19:24:24 -0400

    fix: stabilize path in `useReportWebVitals`

--- src/webVitals/index.ts
+++ src/webVitals/index.ts
@@ -1,10 +1,24 @@
 import { usePathname } from 'next/navigation';
 import { useReportWebVitals as useNextReportWebVitals } from 'next/web-vitals';
 import { reportWebVitalsWithPath } from './webVitals';
+import { useCallback, useRef } from 'react';
 export { type WebVitalsMetric } from './webVitals';
-export { AxiomWebVitals } from './components'
+export { AxiomWebVitals } from './components';
 
 export function useReportWebVitals(path?: string) {
-    const pathName = usePathname();
-    useNextReportWebVitals((metric) => reportWebVitalsWithPath(metric, path || pathName));
+  const pathName = usePathname();
+  const stabilizedPath = useRef(path || pathName);
+
+  if (typeof path === 'string' && path !== stabilizedPath.current) {
+    stabilizedPath.current = pathName;
+  } else if (typeof path === 'string' && path === stabilizedPath.current) {
+    stabilizedPath.current = path;
+  }
+
+  const reportWebVitalsFn: Parameters<typeof useNextReportWebVitals>[0] = useCallback(
+    (metric) => reportWebVitalsWithPath(metric, stabilizedPath.current),
+    []
+  );
+
+  useNextReportWebVitals(reportWebVitalsFn);
 }
